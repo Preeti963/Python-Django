7778bb2b26705aefa87272b573e51f165aef1943
Preeti Magar <preetimagar963@gmail.com>
2025-09-02 22:41:28 +0545

    personal info

---
+++ personal details.py
@@ -0,0 +1,12 @@
+#WAP to user input to add all the personl infromation to dictionary
+#dictionary
+personal_info ={}
+
+#for inputs
+personal_info["name"]= input("Enter your name:")
+personal_info["address"] = input("Enter your address:")
+personal_info["email"] = input("Enter your email:")
+personal_info["number"] = input("Enter your number:")
+
+print("\nPersonal information to dictionary")
+print(personal_info)
