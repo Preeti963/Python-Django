61484425bb7c3ea142a74b40b244dbb32cceeb78
Preeti Magar <preetimagar963@gmail.com>
2025-09-02 22:38:54 +0545

    recharge pin
    recharge pin with hidden numbers except last

---
+++ recharge pin.py
@@ -0,0 +1,14 @@
+recharge_pin = "234223 2344213 2323 5657 2234 2343 6767"
+splited_recharge_pin = recharge_pin.split(" ")
+
+encoded_list = []
+
+for i in range(len(splited_recharge_pin)-1):
+    encoded_string = "*" * len(splited_recharge_pin[i])  
+    encoded_list.append(encoded_string)
+
+encoded_list.append(splited_recharge_pin[-1])   # keep last one as it is
+
+output = " ".join(encoded_list)
+
+print(output)   # ✅ print only once, outside the loop
