3f6112464c0f28974457bace344a838111212f96
Preeti Magar <preetimagar963@gmail.com>
2025-09-03 22:46:10 +0545

    Update remove duplicates.py

--- remove duplicates.py
+++ remove duplicates.py
@@ -1,21 +1,21 @@
 #WAP  to remove duplicacy from list [5,6,3,4,5,6,4,3,5,6,7,3]
-# numbers = [5,6,3,4,5,6,4,3,5,6,7,3]
+numbers = [5,6,3,4,5,6,4,3,5,6,7,3]
 
-# unique_num = list(set(numbers))
+unique_num = list(set(numbers))
 
-# print("list:",numbers)
-# print("removing duplicstes",unique_num)
+print("list:",numbers)
+print("removing duplicstes",unique_num)
 
 #without the  type conversion
 
-# numbers = [5,6,3,4,5,6,4,3,5,6,7,3]
-# unique_num =[]
+numbers = [5,6,3,4,5,6,4,3,5,6,7,3]
+unique_num =[]
 
-# for num in numbers:
-#     if num not in unique_num:
-#         unique_num.append(num)
+for num in numbers:
+    if num not in unique_num:
+        unique_num.append(num)
       
-#     print(unique_num)
+    print(unique_num)
 
 #sort the given list [5,6,3,4,5,6,4,3,5,6,7,3] without using sort predefined function
  
